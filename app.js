1f2636911d0b618953ec53c8447bd35c39a46097
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-06-10 14:49:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var no1=0;
+
+document.getElementById("result").innerText=no1;
+
+function button1()
+{
+    
+     no1=no1+1;
+     document.getElementById("result").innerText=no1;
+}
+
+function button2()
+{
+    no1=no1-1;
+    document.getElementById("result").innerText=no1;
+}
+
